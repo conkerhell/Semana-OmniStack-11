1e95d70d4eaf5dc1ac66a6ffae6739f298edda67
Conkerhell <conker_hell@hotmail.com>
2020-03-25 00:08:09 -0300

    hello world

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return  response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Leonardo Ines'
+    });
+});
 
 app.listen(3333);
 
