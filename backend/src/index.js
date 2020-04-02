e8cb9362e8bdcce904a70f437af72ad48417baa6
Conkerhell <conker_hell@hotmail.com>
2020-04-02 00:47:58 -0300

    Back-End

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,10 +23,11 @@
 
 const express = require('express');
 const routes = require('./routes');
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
